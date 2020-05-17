0c5a6d971648d4637ffed2410ba98fa4268c89db
Christian Studer <cstuder@existenz.ch>
2020-05-17 22:32:58 +0200

    Metadata: Adds initial import of parser

--- src/MetadataParser.php
+++ src/MetadataParser.php
@@ -9,9 +9,46 @@ class MetadataParser
 {
     public static function parse(string $raw)
     {
-        $metadata = [];
+        $metadata = [
+            'locations' => [],
+            'parameters' => [],
+        ];
 
-        // TODO implement his
+        $lines = explode("\n", $raw);
+
+        // Find locations
+        foreach ($lines as $line) {
+            // Query for station line
+            $parts = [];
+            if (!preg_match("/^([A-Z]{3})[ ]+(.+)[ ]+([0-9]{1,2})°([0-9]{2})'\/([0-9]{1,2})°([0-9]{2})'[ ]+([0-9]+)\/([0-9]+)[ ]+([0-9]+)/", utf8_encode($line), $parts)) continue;
+
+            $location = [
+                'id' => $parts[1],
+                'name' => trim($parts[2]),
+                'lat' => round($parts[5] + ($parts[6] / 60), 2),
+                'lon' => round($parts[3] + ($parts[4] / 60), 2),
+                'chx' => intval($parts[7]),
+                'chy' => intval($parts[8]),
+                'alt' => intval($parts[9]),
+            ];
+
+            $metadata['locations'][] = $location;
+        }
+
+        // Find parameters
+        foreach ($lines as $line) {
+            // Query for parameter line
+            $parts = [];
+            if (!preg_match("/^([a-z0-9]{8})[ ]{5,}(.*)[ ]{5,}(.*)/", utf8_encode($line), $parts)) continue;
+
+            $parameter = [
+                'id' => $parts[1],
+                'unit' => trim($parts[2]),
+                'description' => trim($parts[3]),
+            ];
+
+            $metadata['parameters'][] = $parameter;
+        }
 
         return $metadata;
     }
