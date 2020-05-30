cfd636621b7299134732d3757e451f3ff0ea4d27
Christian Studer <cstuder@existenz.ch>
2020-05-30 21:52:38 +0200

    Tests: Adds quantitative test for VQHA80

--- tests/DataParserTest.php
+++ tests/DataParserTest.php
@@ -4,9 +4,43 @@ use PHPUnit\Framework\TestCase;
 
 class DataParserTest extends TestCase
 {
-    public function testParseData()
+    public function testParseDataVQHA80()
     {
-        // TODO complete this
-        $this->assertTrue(true);
+        $raw = file_get_contents(__DIR__ . '/resources/validData/VQHA80.csv');
+        $data = \cstuder\ParseSwissMetNet\DataParser::parse($raw);
+
+        $this->assertEquals(1740, count($data));
+        $this->assertEquals(20, count($this->collectParameters($data)));
+        $this->assertEquals(159, count($this->collectLocations($data)));
+        $this->assertEquals(1, count($this->collectTimestamps($data)));
+    }
+
+
+
+    private function collectParameters($data)
+    {
+        $allParameters = array_map(function ($d) {
+            return $d->par;
+        }, $data);
+
+        return array_unique($allParameters);
+    }
+
+    private function collectLocations($data)
+    {
+        $allLocations = array_map(function ($d) {
+            return $d->loc;
+        }, $data);
+
+        return array_unique($allLocations);
+    }
+
+    private function collectTimestamps($data)
+    {
+        $allTimestamps = array_map(function ($d) {
+            return $d->timestamp;
+        }, $data);
+
+        return array_unique($allTimestamps);
     }
 }
